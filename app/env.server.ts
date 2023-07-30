2f37328e5e0f288ed75c0b4a34eb7c16eda6ce17
Deep <deepkumarmendapara@gmail.com>
2023-07-30 18:36:46 -0500

    feat: set Env variable

--- app/env.server.ts
+++ app/env.server.ts
@@ -1,5 +1,5 @@
 export const env = {
   DATABASE_URL:
     "mongodb+srv://demo:j9hKG8tneGR8duGu@cluster0.kbovzuj.mongodb.net/cep-summer-2023",
-  SESSION_SECRET: "575d8bf1202cc5493027f21bdee5e814",
+  SESSION_SECRET: "my-session-secret",
 }
