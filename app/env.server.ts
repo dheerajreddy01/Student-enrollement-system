06bb6eaefa2215fc22818e5c74307d7d78056d23
Deep <deepkumarmendapara@gmail.com>
2023-07-30 18:33:14 -0500

    feat: set env

---
+++ app/env.server.ts
@@ -0,0 +1,5 @@
+export const env = {
+  DATABASE_URL:
+    "mongodb+srv://demo:j9hKG8tneGR8duGu@cluster0.kbovzuj.mongodb.net/cep-summer-2023",
+  SESSION_SECRET: "575d8bf1202cc5493027f21bdee5e814",
+}
--- app/session.server.ts
+++ app/session.server.ts
@@ -1,9 +1,10 @@
-import { createCookieSessionStorage, redirect } from "@remix-run/node";
-import * as bcrypt from "bcryptjs";
-import invariant from "tiny-invariant";
-import { UserRole } from "~/roles";
+import { createCookieSessionStorage, redirect } from "@remix-run/node"
+import * as bcrypt from "bcryptjs"
+import invariant from "tiny-invariant"
+import { env } from "~/env.server"
+import { UserRole } from "~/roles"
 
-invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
+invariant(env.SESSION_SECRET, "SESSION_SECRET must be set")
 
 export const sessionStorage = createCookieSessionStorage({
   cookie: {
@@ -11,18 +12,18 @@ export const sessionStorage = createCookieSessionStorage({
     httpOnly: true,
     path: "/",
     sameSite: "lax",
-    secrets: [process.env.SESSION_SECRET],
+    secrets: [env.SESSION_SECRET],
     secure: process.env.NODE_ENV === "production",
   },
-});
+})
 
-const USER_SESSION_KEY = "userId";
-const USER_ROLE_KEY = "role";
-const thirtyDaysInSeconds = 60 * 60 * 24 * 30;
+const USER_SESSION_KEY = "userId"
+const USER_ROLE_KEY = "role"
+const thirtyDaysInSeconds = 60 * 60 * 24 * 30
 
 export async function getSession(request: Request) {
-  const cookie = request.headers.get("Cookie");
-  return sessionStorage.getSession(cookie);
+  const cookie = request.headers.get("Cookie")
+  return sessionStorage.getSession(cookie)
 }
 
 export async function createUserSession({
@@ -31,15 +32,15 @@ export async function createUserSession({
   redirectTo,
   role,
 }: {
-  request: Request;
-  userId: string;
-  remember: boolean;
-  redirectTo: string;
-  role: string;
+  request: Request
+  userId: string
+  remember: boolean
+  redirectTo: string
+  role: string
 }) {
-  const session = await getSession(request);
-  session.set(USER_SESSION_KEY, userId);
-  session.set(USER_ROLE_KEY, role);
+  const session = await getSession(request)
+  session.set(USER_SESSION_KEY, userId)
+  session.set(USER_ROLE_KEY, role)
 
   return redirect(redirectTo, {
     headers: {
@@ -47,56 +48,56 @@ export async function createUserSession({
         maxAge: thirtyDaysInSeconds,
       }),
     },
-  });
+  })
 }
 
 export async function getUserId(request: Request): Promise<string | undefined> {
-  const session = await getSession(request);
-  const userId = session.get(USER_SESSION_KEY);
-  return userId;
+  const session = await getSession(request)
+  const userId = session.get(USER_SESSION_KEY)
+  return userId
 }
 
 export async function getUserRole(
-  request: Request
+  request: Request,
 ): Promise<UserRole | undefined> {
-  const session = await getSession(request);
-  const userRole = session.get(USER_ROLE_KEY);
-  return userRole;
+  const session = await getSession(request)
+  const userRole = session.get(USER_ROLE_KEY)
+  return userRole
 }
 
 export async function requireUserId(request: Request) {
-  const userId = await getUserId(request);
+  const userId = await getUserId(request)
   if (!userId) {
-    throw redirect("/");
+    throw redirect("/")
   }
 
-  return userId;
+  return userId
 }
 
 export async function logout(request: Request) {
-  const session = await getSession(request);
+  const session = await getSession(request)
   return redirect("/", {
     headers: {
       "Set-Cookie": await sessionStorage.destroySession(session),
     },
-  });
+  })
 }
 
 export function createPasswordHash(password: string) {
-  return bcrypt.hash(password, 10);
+  return bcrypt.hash(password, 10)
 }
 
 export async function isAdmin(request: Request) {
-  const session = await getSession(request);
-  return session.get(USER_ROLE_KEY) === UserRole.ADMIN;
+  const session = await getSession(request)
+  return session.get(USER_ROLE_KEY) === UserRole.ADMIN
 }
 
 export async function isFaculty(request: Request) {
-  const session = await getSession(request);
-  return session.get(USER_ROLE_KEY) === UserRole.FACULTY;
+  const session = await getSession(request)
+  return session.get(USER_ROLE_KEY) === UserRole.FACULTY
 }
 
 export async function isStudent(request: Request) {
-  const session = await getSession(request);
-  return session.get(USER_ROLE_KEY) === UserRole.STUDENT;
+  const session = await getSession(request)
+  return session.get(USER_ROLE_KEY) === UserRole.STUDENT
 }
